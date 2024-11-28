39cee7536ac9fd40b468bfb02309a2eea55cef50
Anu Uprety <anu6@vt.edu>
2024-11-27 21:33:21 -0500

    Update Array and Loop Itteration.py
    prints the number in range of the loop + 1

--- Array and Loop Itteration.py
+++ Array and Loop Itteration.py
@@ -6,3 +6,16 @@ for i in numbers:
         print('iteration', cnt, end=': ')
         print(j)
         cnt = cnt + 1
+
+
+# will provide the following output, note the block comment not actual data from the algorithim above:
+
+'''
+#PS C:\Users\oakto\.vscode\extensions> & C:/Users/oakto/AppData/Local/Programs/Python/Python313/python.exe "c:/Users/oakto/OneDrive/Documents/PyGuy/Array and Loop Itteration.py"
+iteration 0: 1
+iteration 1: 2
+iteration 2: 3
+iteration 3: 4
+iteration 4: 5
+iteration 5: 6
+'''
