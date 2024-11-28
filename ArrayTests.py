7548ab5ce85db5a65df6f304aae072ca39f0c999
Anu Uprety <anu6@vt.edu>
2024-11-28 03:06:59 -0500

    Update ArrayTests.py

--- ArrayTests.py
+++ ArrayTests.py
@@ -4,3 +4,11 @@ print("Original array: "+str(array_num))
 print("Remove the first occurrence of 3 from the said array:")
 array_num.remove(3)
 print("New array: "+str(array_num))
+
+#Compiled Code:
+
+#PS C:\Users\oakto\.vscode\extensions> & C:/Users/oakto/OneDrive/Documents/PyGuy/python.exe c:/Users/oakto/OneDrive/Documents/PyGuy/ArrayTests.py
+#Original array: array('i', [1, 3, 5, 3, 7, 1, 9, 3])
+#Remove the first occurrence of 3 from the said array:
+#New array: array('i', [1, 5, 3, 7, 1, 9, 3])
+#PS C:\Users\oakto\.vscode\extensions> 
